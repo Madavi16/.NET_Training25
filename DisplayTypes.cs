088fdf892fe87883ca49981bab85af4eccf91f85
Madavi <madhavirm007@gmail.com>
2025-10-31 17:53:11 +0530

    Add files via upload

---
+++ DisplayTypes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FirstPrj
+{
+    internal class DisplayTypes
+    {
+        public void Display()
+        {
+            string fname, lname;
+            Console.WriteLine("Enter your first name:");
+            fname = Console.ReadLine();
+            Console.WriteLine("Enter your last name:");
+            lname = Console.ReadLine();
+
+            Console.WriteLine($"Your first name is : {fname} and your last name is : {lname}");
+            Console.WriteLine("Your first name is: " + fname + " and your last name is: " + lname);
+            Console.WriteLine("Your first name is: {0} and your last name is: {1}", fname, lname);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace FirstPrj
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello..");
+
+            Program program = new Program();
+            int c = program.add(5, 6);
+            Console.WriteLine(c);
+
+            Program program1 = new Program();
+            int d = program1.add(31, 32);
+            Console.WriteLine(d);
+
+            Greet();
+
+            NewClass.Message("Infinite");
+
+            NewClass newClass = new NewClass();
+            Console.WriteLine(newClass.Hello("Madhavi"));
+
+            DisplayTypes dt = new DisplayTypes();
+            dt.Display();
+
+            Console.ReadLine();
+        }
+
+        int add(int x, int y)
+        {
+            Console.WriteLine("Hii reached");
+            return x + y;
+        }
+
+        static void Greet()
+        {
+            Console.WriteLine("Hii");
+        }
+    }
+
+    class NewClass
+    {
+        public static void Message(String name)
+        {
+            Console.WriteLine(name);
+        }
+
+        public string Hello(String name)
+        {
+            return "Hi " + name;
+        }
+    }
+    
+}
+
