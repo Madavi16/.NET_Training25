1617f25194b6a7d8d1967cd3878b05a4ac1b762a
Madavi Ravi <madavir@infinite.com>
2025-12-29 11:31:11 +0530

    Electricity Project

---
+++ ElectricityProject/App_Code/BillValidator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Configuration;
+
+
+namespace ElectricityProject.App_Code
+{
+    public class BillValidator
+    {
+        public string ValidateUnitsConsumed(int unitsConsumed)
+        {
+            if (unitsConsumed < 0)
+            {
+                return "Given units is invalid";
+            }
+            return "Valid";
+        }
+    }
+}
---
+++ ElectricityProject/Login.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace ElectricityProject
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            string username = txtUser.Text;
+            string password = txtPass.Text;
+
+            if (username == "Madavi" && password == "67845")
+            {
+                Session["user"] = username;
+
+                Response.Redirect("AddBill.aspx");
+            }
+            else
+            {
+                lblMsg.Text = "Invalid Username or Password";
+            }
+        }
+    }
+}
