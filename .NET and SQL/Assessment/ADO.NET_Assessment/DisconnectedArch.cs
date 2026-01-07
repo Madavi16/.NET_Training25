cc33abc871cd453431c9b34487f23244b1e04a5a
Madavi Ravi <madavir@infinite.com>
2026-01-07 10:02:43 +0530

    Committing Files

---
+++ .NET and SQL/Assessment/ADO.NET_Assessment/DisconnectedArch.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADO.NET_Assign
+{
+    internal class DisconnectedArch
+    {
+        
+
+        private string connection = "Integrated Security = true;MultipleActiveResultSets=true; database = EduTrack; server =(localdb)\\MSSQLLocalDB";
+
+        private DataSet ds = new DataSet();
+        private SqlDataAdapter daStudent;
+        private SqlDataAdapter daCourses;
+        private DataTable dt = new DataTable();
+        private DataTable dt2 = new DataTable();
+
+
+        public void DisconnectedEduTrack()
+        {
+            SqlConnection con=new SqlConnection(connection);
+            daStudent = new SqlDataAdapter("Select * from Students", con);
+            daCourses = new SqlDataAdapter("Select * from Courses ", con);
+
+            daStudent.Fill(ds, "Students");
+            daCourses.Fill(ds, "Courses");
+
+            daStudent.MissingSchemaAction = MissingSchemaAction.AddWithKey;
+            daCourses.MissingSchemaAction = MissingSchemaAction.AddWithKey;
+
+            ds.Tables["Courses"].PrimaryKey = new DataColumn[] { ds.Tables["Courses"].Columns["CourseId"] };
+            ds.Tables["Courses"].Columns["CourseId"].AutoIncrement = true;
+
+            ds.Tables["Students"].PrimaryKey = new DataColumn[] { ds.Tables["Students"].Columns["StudentId"] };
+            ds.Tables["Students"].Columns["StudentId"].AutoIncrement = true;
+
+
+            SqlCommandBuilder cb = new SqlCommandBuilder(daStudent);
+            SqlCommandBuilder cb1 = new SqlCommandBuilder(daCourses);
+
+        }
+
+        //1st 3.1
+        public void LoadDetails()
+        {
+
+            Console.WriteLine("Students Table: ");
+            dt = ds.Tables["Students"];
+            foreach(DataRow r in dt.Rows)
+            {
+                Console.WriteLine($"{r[0]}\t{r[1]}\t{r[2]}\t{r[3]}");
+            }
+        }
+
+        //2nd 3.2
+        public void UpdateCredits()
+        {
+            Console.WriteLine("Enter CourseID to Update credits: ");
+            int id = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter new Credits: ");
+            int credits=int.Parse(Console.ReadLine());
+
+            dt2 = ds.Tables["Courses"];
+            DataRow row = dt2.Rows.Find(id);
+            
+            if(row!=null)
+            {
+                row["Credits"] =credits;
+                
+                daCourses.Update(ds, "Courses");
+                Console.WriteLine("Course credits updated successfully.");
+                
+            }
+            else
+            {
+                Console.WriteLine("Course not found");
+            }           
+
+        }
+
+        //3rd 3.3
+        public void AddCourse()
+        {
+           
+
+            Console.WriteLine("Course Name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter Credits: ");
+            int credits=int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Semester: ");
+            string sem=Console.ReadLine();
+
+            dt2 = ds.Tables["Courses"];
+            DataRow newRow = dt2.NewRow();
+
+            newRow["CourseName"] = name;
+            newRow["Credits"] = credits;
+            newRow["Semester"] = sem;
+
+            dt2.Rows.Add(newRow);
+            daCourses.Update(ds, "Courses");
+
+            Console.WriteLine("New course inserted Successfully.");
+           
+        }
+
+        //4th 3.4
+        public void DeleteStudent()
+        {
+            Console.WriteLine("Enter student Id to delete: ");
+            int id=int.Parse(Console.ReadLine());
+
+            dt = ds.Tables["Students"];
+            DataRow row= dt.Rows.Find(id);
+            if(row!=null)
+            {
+                row.Delete();
+                daStudent.Update(ds, "Students");
+                Console.WriteLine("Student Record Deleted Successfully");
+            }
+            else
+            {
+                Console.WriteLine("Student not found");
+            }
+
+        }
+
+
+    }
+}
---
+++ .NET and SQL/Assignment/ENTITY_Framework/EmpCurdDemo.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Configuration;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Code_frist_Demo
+{
+    internal class EmpCurdDemo
+    {
+
+        Model1 db = new Model1();
+        public void InsertEmployee()
+        {
+            Console.WriteLine("How many employees do you want to insert?");
+            int count = int.Parse(Console.ReadLine());
+
+            try
+            {
+
+                for (int i = 1; i <= count; i++)
+                {
+                    Console.WriteLine($"\nEnter details for Employee {i}");
+
+                    Console.Write("EmpId: ");
+                    string empId = Console.ReadLine();
+
+                    Console.Write("Emp Name: ");
+                    string empName = Console.ReadLine();
+
+                    Console.Write("Department Name: ");
+                    string deptName = Console.ReadLine();
+
+                    Console.Write("Salary: ");
+                    int salary = int.Parse(Console.ReadLine());
+
+                    Console.Write("Year Of Joining: ");
+                    int year = int.Parse(Console.ReadLine());
+
+                    EmployeeEf emp = new EmployeeEf()
+                    {
+                        EmpId = empId,
+                        EmpName =empName,
+                        DepartmentName = deptName,
+                        Salary = salary,
+                        YearOfJoining = year
+
+                    };
+
+                    db.employees.Add(emp);
+                    int rowAff = db.SaveChanges();
+                    Console.WriteLine($"Records inserted: {rowAff}");
+                }
+            }
+            catch
+            {
+                var res = db.GetValidationErrors();
+
+                foreach(var item in res)
+                {
+                    if(item.ValidationErrors.Count>0)
+                    {
+                        foreach(var err in item.ValidationErrors)
+                        {
+                            Console.WriteLine(err.ErrorMessage);
+                        }
+                    }
+                }
+
+            }
+
+
+        }
+
+        public void DisplayAll()
+        {
+            var res = db.employees.ToList();
+            foreach(var e in res)
+            {
+                Console.WriteLine($"{e.EmpId}\t{e.EmpName}\t{e.DepartmentName}\t{e.Salary}\t{e.YearOfJoining}");
+
+            }
+            Console.WriteLine();
+        }
+
+        public void SearchEmployeebyId()
+        {
+            Console.WriteLine("Enter EmpId: ");
+            string id = Console.ReadLine();
+
+            var emp=db.employees.FirstOrDefault(e => e.EmpId == id);
+
+            if(emp==null)
+            {
+                Console.WriteLine("Employee Not found");
+                return;
+            }
+
+            Console.WriteLine($"{emp.EmpId}\t{emp.EmpName}\t{emp.DepartmentName}\t{emp.Salary}\t{emp.YearOfJoining}");
+        }
+
+        public void UpdateSalary()
+        {
+            Console.WriteLine("Enter emp id: ");
+            string id= Console.ReadLine();
+
+            var emp = db.employees.FirstOrDefault(e => e.EmpId == id);
+
+            if(emp==null)
+            {
+                Console.WriteLine("Employee Not Found");
+                return;
+
+            }
+
+            Console.WriteLine("Enter new Salary:");
+           emp.Salary=int.Parse(Console.ReadLine());
+           int res= db.SaveChanges();
+            Console.WriteLine($"No of rows affected:{res}");
+            Console.WriteLine("Salary updated successfully");
+        }
+
+        public void DeleteEmp()
+        {
+            Console.WriteLine("Enter Empid to delete: ");
+            string id = Console.ReadLine();
+
+            var emp = db.employees.FirstOrDefault(e => e.EmpId == id);
+
+            if(emp==null)
+            {
+                Console.WriteLine("Employees not found");
+                return;
+            }
+
+
+            db.employees.Remove(emp);
+            int res=db.SaveChanges();
+            Console.WriteLine($"Rows Affected:{res}");
+            Console.WriteLine("Employee deleted successfully.");
+        }
+
+
+
+    }
+}
